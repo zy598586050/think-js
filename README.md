c4885f9810c48709e91d8120a8883feb01e6ec31
zhangyu <yu.zhang@simceredx.com>
2021-08-06 10:28:58 +0800

    [v1.0.2]

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 <!--
  * @Author: zhangyu
  * @Date: 2021-07-28 20:42:08
- * @LastEditTime: 2021-07-28 20:45:11
+ * @LastEditTime: 2021-08-06 10:28:15
 -->
 ### ThinkJS框架
 
 ThinkJS是一个企业级的NodeJS应用框架，为提高开发团队和开发人员的开发效率而生。
+
+> V1.0.2
+
+* 增加session配置
+* 升级think-js-lib版本1.0.4
 
 > V1.0.1
 
--- config/app.js
+++ config/app.js
@@ -1,10 +1,20 @@
 /*
  * @Author: zhangyu
  * @Date: 2021-04-14 21:28:08
- * @LastEditTime: 2021-04-28 16:13:55
+ * @LastEditTime: 2021-08-06 10:25:14
  */
 
 // 项目配置文件
 module.exports = {
     port: 5985, // 项目启动端口
+    cookie_secret: 'secret', // cookie的签名
+    session_config: {
+        key: 'koa:sess',
+        maxAge: 18000, // cookie的过期时间
+        overwrite: true, // 是否可以overwrite
+        httpOnly: true, // true表示只有服务器端可以获取cookie
+        signed: true, // 签名
+        rolling: true, // 在每次请求时强行设置 cookie，这将重置 cookie 过期时间（默认：false）
+        renew: false, // (boolean) renew session when session is nearly expired
+    } // session配置
 }
