111bc4dab592295192186a80da369ee7c4ab2c60
zhangyu <598586050@qq.com>
2021-08-17 22:38:04 +0800

    [V1.0.3]

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 <!--
  * @Author: zhangyu
  * @Date: 2021-07-28 20:42:08
- * @LastEditTime: 2021-08-06 10:28:15
+ * @LastEditTime: 2021-08-17 21:10:09
 -->
 ### ThinkJS框架
 
 ThinkJS是一个企业级的NodeJS应用框架，为提高开发团队和开发人员的开发效率而生。
+
+> V1.0.3
+
+* middleware写法改变
+* 升级think-js-lib版本1.0.5
 
 > V1.0.2
 
--- app/middleware/checkLogin.js
+++ app/middleware/checkLogin.js
@@ -1,12 +1,21 @@
 /*
  * @Author: zhangyu
  * @Date: 2021-04-20 21:50:39
- * @LastEditTime: 2021-04-29 12:40:28
+ * @LastEditTime: 2021-08-17 21:07:53
  */
+const TR = require('../../node_modules/think-js-lib/lib/thinkredis.js')
+const ThinkRedis = new TR()
 
 // 路由中间件
-const checkLogin = (ctx,next,error) => {
-    next()
+const checkLogin = async (ctx,next,error) => {
+    let userInfo = await ThinkRedis.get(ctx.header.token || '')
+    return new Promise((resolve,reject) => {
+        if(userInfo){
+            resolve(next())
+        }else{
+            reject(error('非法请求,或Token过期'))
+        }
+    })
 }
 
 module.exports = checkLogin
